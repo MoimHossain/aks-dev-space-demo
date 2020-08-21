76efe6d9c91056b4bd69c36f0a7d83d23f05a744
Moim HA <moimhossain@gmail.com>
2020-08-21 11:22:33 +0200

    Added azds install script

---
+++ infrastructure/install-azds.sh
@@ -0,0 +1,9 @@
+
+
+echo $resourceGroup
+echo $location
+echo $clusterName
+echo $spId
+echo $secret
+
+az aks use-dev-spaces -g $resourceGroup -n $clusterName
--- infrastructure/provision.sh
+++ infrastructure/provision.sh
@@ -7,6 +7,4 @@ echo $secret
 
 az group create --name $resourceGroup --location $location
 
-# az aks create --resource-group $resourceGroup --name $clusterName --generate-ssh-keys --service-principal $spId --client-secret $secret
-
-# az aks use-dev-spaces -g $resourceGroup -n $clusterName
+az aks create --resource-group $resourceGroup --name $clusterName --generate-ssh-keys --service-principal $spId --client-secret $secret
